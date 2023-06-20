184d027b58651e2d9c96a43b19cbca5c9fba9f70
Deniel Chiang <deniel@jay-creative.com>
2023-06-20 14:39:59 +0800

    fixed formatting

--- mix.exs
+++ mix.exs
@@ -48,7 +48,7 @@ defmodule LineReminder.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
