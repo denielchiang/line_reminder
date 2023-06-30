e4417ad5b7e4a49d475dd9d704830eb653706b41
Deniel Chiang <deniel@jay-creative.com>
2023-06-30 16:54:59 +0800

    add dializer on test env

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule LineReminder.MixProject do
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:sobelow, "~> 0.12", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.3", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev, :test], runtime: false},
       {:tzdata, "~> 1.1"},
       {:quantum, "~> 3.0"},
       {:heroicons, "~> 0.5.0"},
