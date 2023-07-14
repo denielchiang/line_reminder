c85d5692302df5462b8814ebf1ef68c704119a86
Deniel Chiang <deniel@jay-creative.com>
2023-07-14 11:40:39 +0800

    refactor Req.new/1

--- lib/line_reminder/helpers/line.ex
+++ lib/line_reminder/helpers/line.ex
@@ -2,15 +2,13 @@ defmodule LineReminder.Line do
   @moduledoc """
   Line http client wrapper
   """
-
-  @content_type_key "Content-Type"
-  @content_type_value "application/x-www-form-urlencoded"
-  @line_notify_url "https://notify-api.line.me/api/notify"
-
   @spec init() :: Req.Request.t()
   defp init() do
-    Req.new(url: @line_notify_url)
-    |> Req.Request.put_header(@content_type_key, @content_type_value)
+    Req.new(
+      url: "https://notify-api.line.me/api/notify",
+      headers: [{"Content-Type", "application/x-www-form-urlencoded"}],
+      auth: {:bearer, Application.fetch_env!(:line_reminder, :line_token)}
+    )
   end
 
   @doc """
@@ -19,15 +17,12 @@ defmodule LineReminder.Line do
   ## Examples
 
   iex> send_to_group("abc")
-  {:ok, res}
+  {:ok, message}
   """
   @spec send_to_group(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def send_to_group(msg) do
     init()
-    |> Req.post(
-      auth: {:bearer, Application.fetch_env!(:line_reminder, :line_token)},
-      form: [message: msg]
-    )
+    |> Req.post(form: [message: msg])
     |> case do
       {:ok, %Req.Response{status: 200}} ->
         {:ok, "Topic already be sent"}
