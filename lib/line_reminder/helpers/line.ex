f4284a00922e8501c12dd88eccc40824edff70b3
Deniel Chiang <deniel@jay-creative.com>
2023-07-14 11:21:58 +0800

    refactor url

--- lib/line_reminder/helpers/line.ex
+++ lib/line_reminder/helpers/line.ex
@@ -6,8 +6,10 @@ defmodule LineReminder.Line do
   @content_type_key "Content-Type"
   @content_type_value "application/x-www-form-urlencoded"
   @line_notify_url "https://notify-api.line.me/api/notify"
+
+  @spec init() :: Req.Request.t()
   defp init() do
-    Req.new()
+    Req.new(url: @line_notify_url)
     |> Req.Request.put_header(@content_type_key, @content_type_value)
   end
 
@@ -22,8 +24,6 @@ defmodule LineReminder.Line do
   @spec send_to_group(String.t()) :: {:ok, String.t()} | {:error, String.t()}
   def send_to_group(msg) do
     init()
-
-    @line_notify_url
     |> Req.post(
       auth: {:bearer, Application.fetch_env!(:line_reminder, :line_token)},
       form: [message: msg]
