fe1e9d1130a8ce50e8fa855df868f400de872dab
deniel <deniel@room3327.net>
2024-01-07 01:31:33 +0800

    downgrade fly.io machine

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :line_reminder, LineReminderWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
---
+++ priv/repo/seeds.exs
@@ -0,0 +1,2 @@
+Code.eval_file("./priv/repo/seeds_2023.exs")
+Code.eval_file("./priv/repo/seeds_2024.exs")
