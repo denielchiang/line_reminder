b67738927c9720949cbb83107216fe9079a29e28
deniel <deniel@room3327.net>
2024-05-08 11:05:07 +0800

    excel helper refactor

--- lib/line_reminder/helpers/excel_helper.ex
+++ lib/line_reminder/helpers/excel_helper.ex
@@ -1,35 +1,96 @@
 defmodule LineReminder.ExcelHelper do
+  @moduledoc """
+  This module provides functionality to import data from Excel files, specifically targeting
+  certain sheet types defined by the `@sheet_types` module attribute.
+
+  The imported data is transformed and filtered based on various conditions, such as the
+  presence of `NaiveDateTime` values and the number of `nil` values in each row. The resulting
+  data is returned as a list of maps, where each map represents a row from the Excel sheet with
+  keys for the name, date, and group.
+
+  ## Examples
+
+      iex> LineReminder.ExcelHelper.import_all("path/to/file.xlsx")
+      [
+        %{name: "Chapter1", date: ~N[2023-05-01 00:00:00], group: 1},
+        %{name: "Chapter2", date: ~N[2023-05-02 00:00:00], group: 2},
+        %{name: "Chapter3", date: ~N[2023-05-03 00:00:00], group: 3}
+      ]
+  """
+  require Logger
+
+  @typedoc """
+  A map representing the sheet types and their corresponding integer values.
+  """
+  @type sheet_types_map :: %{
+          general: 0,
+          advanced: 1,
+          companion: 2
+        }
   @sheet_types %{
     general: 0,
     advanced: 1,
     companion: 2
   }
 
+  @doc """
+  Imports data from all sheet types defined in `@sheet_types`.
+
+  ## Parameters
+
+    - `file_path`: A string representing the path to the Excel file.
+
+  ## Returns
+
+    - A list of maps, where each map represents a row from the Excel sheet with keys for the name, date, and group.
+  """
+  @spec import_all(String.t()) :: [map()]
   def import_all(file_path) do
     @sheet_types
     |> Enum.map(&import_sheet(file_path, &1))
     |> List.flatten()
   end
 
-  def import_sheet(file_path, {group, sheet_idx}) do
-    with {:parser, {:ok, tid}} <- {:parser, extract_sheet(file_path, sheet_idx)} do
-      sheet = Xlsxir.get_list(tid)
-      Xlsxir.close(tid)
+  @doc """
+  Imports data from a specific sheet type in the Excel file.
 
+  ## Parameters
+
+    - `file_path`: A string representing the path to the Excel file.
+    - `{group, sheet_idx}`: A tuple containing the sheet type atom (`:general`, `:advanced`, or `:companion`) and the sheet index.
+
+  ## Returns
+
+    - A list of maps, where each map represents a row from the specified sheet with keys for the name, date, and group.
+    - `{:error, reason}` if there was an error importing the sheet.
+  """
+  @spec import_sheet(String.t(), {atom(), non_neg_integer()}) ::
+          [map()] | {:error, String.t()}
+  def import_sheet(file_path, {group, sheet_idx}) do
+    with {:ok, tid} <- extract_sheet(file_path, sheet_idx),
+         {:xlsxir, sheet} <- {:xlsxir, Xlsxir.get_list(tid)},
+         {:ets, :ok} <- {:ets, Xlsxir.close(tid)} do
       sheet
-      |> Enum.map(&Enum.count(&1, fn cell -> is_nil(cell) end))
-      |> then(&Enum.zip(sheet, &1))
-      |> Enum.reject(&incorrect_nil_counts/1)
-      |> Enum.filter(&valuable_data?/1)
-      |> Enum.map(&reshaping/1)
-      |> Enum.map(&trim_cell/1)
-      |> List.flatten()
-      |> Enum.chunk_every(2)
+      |> Stream.map(&Stream.map(&1, fn cell -> if is_nil(cell), do: 1, else: 0 end))
+      |> Stream.map(&Enum.sum/1)
+      |> Enum.to_list()
+      |> then(&Stream.zip(sheet, &1))
+      |> Stream.reject(&incorrect_nil_counts/1)
+      |> Stream.filter(&valuable_data?/1)
+      |> Stream.map(&reshaping/1)
+      |> Stream.map(&trim_cell/1)
+      |> Stream.flat_map(& &1)
+      |> Stream.chunk_every(2)
+      |> Enum.to_list()
       |> Enum.map(&build_map(&1, group))
+    else
+      {:error, reason} ->
+        Logger.error("Import excel failed: #{reason}")
+        {:error, reason}
     end
   end
 
-  defp build_map([date, chapter], group) do
+  def build_map([date, chapter], group) do
     %{
       name: chapter,
       date: date,
@@ -37,7 +98,7 @@ defmodule LineReminder.ExcelHelper do
     }
   end
 
-  defp fit_structs_group(group) do
+  def fit_structs_group(group) do
     case group do
       :general -> 1
       :advanced -> 2
@@ -45,9 +106,9 @@ defmodule LineReminder.ExcelHelper do
     end
   end
 
-  defp extract_sheet(nil, _idx), do: {:parser, {:error, "path is nil"}}
+  def extract_sheet(nil, _idx), do: {:parser, {:error, "path is nil"}}
 
-  defp extract_sheet(file_path, idx) do
+  def extract_sheet(file_path, idx) do
     if File.exists?(file_path) do
       Xlsxir.multi_extract(file_path, idx, false, max_rows: 200)
     else
---
+++ test/line_reminder/helpers/excel_helper_test.exs
@@ -0,0 +1,89 @@
+defmodule LineReminder.ExcelHelperTest do
+  use LineReminder.DataCase, async: true
+
+  @moduletag capture_log: true
+
+  alias LineReminder.ExcelHelper
+  alias LineReminder.Events
+
+  @test_file_path "/Users/denielchiang/Develop/temp/Reading_plan_2024_test.xlsx"
+
+  describe "import_all/1" do
+    test "imports data from all sheet types" do
+      expected_output = [
+        %{name: "Chapter1", date: ~N[2023-05-01 00:00:00], group: 1},
+        %{name: "Chapter2", date: ~N[2023-05-02 00:00:00], group: 1},
+        %{name: "Chapter3", date: ~N[2023-05-03 00:00:00], group: 1},
+        %{name: "Chapter4", date: ~N[2023-05-04 00:00:00], group: 2},
+        %{name: "Chapter5", date: ~N[2023-05-05 00:00:00], group: 2},
+        %{name: "Chapter6", date: ~N[2023-05-06 00:00:00], group: 2},
+        %{name: "Chapter7", date: ~N[2023-05-07 00:00:00], group: 3},
+        %{name: "Chapter8", date: ~N[2023-05-08 00:00:00], group: 3},
+        %{name: "Chapter9", date: ~N[2023-05-09 00:00:00], group: 3}
+      ]
+
+      assert ExcelHelper.import_all(@test_file_path) == expected_output
+    end
+  end
+
+  describe "import_sheet/2" do
+    test "imports data from a specific sheet type" do
+      expected_output = [
+        %{name: "Chapter1", date: ~N[2023-05-01 00:00:00], group: 1},
+        %{name: "Chapter2", date: ~N[2023-05-02 00:00:00], group: 1},
+        %{name: "Chapter3", date: ~N[2023-05-03 00:00:00], group: 1}
+      ]
+
+      assert ExcelHelper.import_sheet(@test_file_path, {:general, 0}) == expected_output
+    end
+
+    test "returns an empty list for an invalid sheet index" do
+      expected_output = {:error, "Invalid worksheet index."}
+      assert ExcelHelper.import_sheet(@test_file_path, {:general, 100}) == expected_output
+    end
+
+    test "import 9 rows of events data" do
+      events = ExcelHelper.import_all(@test_file_path)
+
+      Repo.transaction(fn ->
+        Enum.each(events, fn event ->
+          Events.create_event(event)
+        end)
+      end)
+
+      quried_events = Events.list_events()
+      assert length(quried_events) == 9
+    end
+  end
+
+  describe "build_map/2" do
+    test "builds a map with the correct keys and values" do
+      date = ~N[2023-05-01 00:00:00]
+      chapter = "Chapter1"
+      group = :general
+
+      expected_output = %{name: "Chapter1", date: ~N[2023-05-01 00:00:00], group: 1}
+
+      assert ExcelHelper.build_map([date, chapter], group) == expected_output
+    end
+  end
+
+  describe "fit_structs_group/1" do
+    test "returns the correct integer value for each group" do
+      assert ExcelHelper.fit_structs_group(:general) == 1
+      assert ExcelHelper.fit_structs_group(:advanced) == 2
+      assert ExcelHelper.fit_structs_group(:companion) == 3
+    end
+  end
+
+  describe "extract_sheet/2" do
+    test "returns an error tuple for a nil file path" do
+      assert ExcelHelper.extract_sheet(nil, 0) == {:parser, {:error, "path is nil"}}
+    end
+
+    test "returns an error tuple for a non-existent file" do
+      assert ExcelHelper.extract_sheet("non_existent_file.xlsx", 0) ==
+               {:error, "file is not exist"}
+    end
+  end
+end
